8f1ebf3780ad26aaa4a256d737c3cdf014deab26
manita <manitamaharjan055@gmail.com>
2021-10-31 22:35:55 +0545

    first project

--- a.py
+++ a.py
@@ -10,10 +10,10 @@ print('The even numbers are:')
 for num in range(0,21,2):
     print( num, end = ' ')
 
-
-for item in range:
-    print(item = int((input('Enter a number: '))))
+num = int((input('Enter a number: ')))
+for item in range(1, num + 1):
     if num % 2 == 0:
         print('It is even number.')
     else:
         print('It is not even number.' )
+    
