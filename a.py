2a49bb836c787bf27651b684d20bd51ac36cc5b2
manita <manitamaharjan055@gmail.com>
2021-10-31 21:38:37 +0545

    test

---
+++ a.py
@@ -0,0 +1,19 @@
+#print even number
+n = int((input('Enter a number: ')))
+if n % 2 == 0:
+        print( 'It is even number.')
+else:
+        print('It is not even number.' )
+
+#print even number between 0 to 20  
+print('The even numbers are:')
+for num in range(0,21,2):
+    print( num, end = ' ')
+
+
+for item in range:
+    print(item = int((input('Enter a number: '))))
+    if num % 2 == 0:
+        print('It is even number.')
+    else:
+        print('It is not even number.' )
